6f842957ad45a0894e7ae3e0a41877d901e0f41e
soronnadi thedeus <soronnadiogochukwu@gmail.com>
2026-09-02 22:02:39 +0100

    Fix SMTP Gmail connection

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,14 +6,14 @@ const nodemailer = require("nodemailer");
 // EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
 });
-
 // REGISTER USER
 const registerUser = async (req, res) => {
   try {
