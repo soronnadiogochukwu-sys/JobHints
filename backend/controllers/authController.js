db4b51733be230e7255ad9c695ce59cff0891afe
soronnadi thedeus <soronnadiogochukwu@gmail.com>
2026-09-02 17:01:26 +0100

    fix auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,9 @@ const nodemailer = require("nodemailer");
 
 // EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
